aee03eaaa852b24a5886e57a31b82f47b55b47e6
Ma7m0ud3id127 <mahmoudeid127127@gmail.com>
2022-09-23 11:10:14 +0200

    Ex7

---
+++ Embedded_System_diploma/unit2__lesson3/EX7.c
@@ -0,0 +1,28 @@
+/*
+ ============================================================================
+ Name        : EX8.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	            float a,b,test=0;
+				printf("Enter value of a:");
+				fflush(stdin);fflush(stdout);
+				scanf("%f",&a);
+				printf("Enter value of b:");
+				fflush(stdin);fflush(stdout);
+				scanf("%f",&b);
+				b+=a;
+				a=b-a;
+				b=b-a;
+				printf("After swapping, value of a =%f\r\n",a);
+				printf("After swapping, value of b =%f",b);
+				return 0 ;
+}
