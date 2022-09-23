ba37de3ba94b6f36878529e0b817f83edc784e3c
Ma7m0ud3id127 <mahmoudeid127127@gmail.com>
2022-09-23 11:03:45 +0200

    Ex4

---
+++ Embedded_System_diploma/unit2__lesson3/EX4.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : EX4.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	float x,y;
+		printf("Enter two numbers:");
+		fflush(stdin);fflush(stdout);
+		scanf("%f",&x);
+		scanf("%f",&y);
+		printf("Product:%f",x*y);
+		return 0 ;
+}
